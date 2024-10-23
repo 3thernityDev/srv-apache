8ab9047789bc87fd2c3854920d6546bb3bdda357
3thernitydev <3thernitydev@gmail.com>
2024-10-23 09:39:44 +0400

    variables test

--- website/index.js
+++ website/index.js
@@ -3,3 +3,18 @@
 
 //  Console Log
 console.log("Welcome in this javascript test :)");
+
+// Utilisation de variables
+
+const setPseudo = () => {
+    let pseudo = "3thernity";
+    console.log(pseudo);
+    document.getElementById("pseudo").innerText = pseudo;
+};
+
+setPseudo([]);
+// Utilisation typeof
+const toto = 1;
+console.log(typeof toto);
+
+// Object
